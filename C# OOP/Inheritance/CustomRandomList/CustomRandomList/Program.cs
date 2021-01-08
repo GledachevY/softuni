5c592abee743aa9bdd264ca1531fb052d2c6af25
Yavor Gledachev <jaking0111@gmail.com>
2021-01-08 14:08:20 +0200

    new exercise random list

---
+++ C# OOP/Inheritance/CustomRandomList/CustomRandomList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+  public  class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList mylist = new RandomList();
+            mylist.Add("adasdas");
+            mylist.Add("asdas");
+            mylist.Add("hfgh");
+            mylist.RandomString();
+        }
+    }
+}
