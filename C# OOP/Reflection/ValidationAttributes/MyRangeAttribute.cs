a6a2b2d7f31cba61f13d2fbc2446549bb38b24f9
Yavor Gledachev <jaking0111@gmail.com>
2021-01-26 15:08:58 +0200

    reflection exercise

---
+++ C# OOP/Reflection/ValidationAttributes/MyRangeAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private readonly int _min;
+        private readonly int _max;
+        public MyRangeAttribute(int min, int max)
+        {
+            this._max = max;
+            this._min = min;
+        }
+        public override bool IsValid(object obj)
+        {
+            if(!(obj is int))
+            {
+                throw new ArgumentException();
+            }
+
+            int valueAsInt = (int)obj;
+
+            if(valueAsInt >= _min && valueAsInt <= _max)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ C# OOP/Reflection/ValidationAttributes/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    class Person
+    {
+        public Person(string fullName, int age)
+        {
+            this.FullName = fullName;
+            this.Age = age;
+        }
+        
+        [MyRange(12, 90)]
+        public int Age { get; set; }
+        [MyRequired]
+        public string FullName { get; set; }
+    }
+}
