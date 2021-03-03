b093a4aebe432df551a68e5270fff4f3ea89cc8a
Yavor Gledachev <jaking0111@gmail.com>
2021-03-03 14:41:12 +0200

    add exercise for code first

---
+++ Entity Framewor Core/CodeFirst/HospitalDatabase/Data/HospitalContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext()
+        {
+
+        }
+        public HospitalContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Diagnose> Diagnoses { get; set; }
+        public DbSet<Medicament> Medicaments{ get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<PatientMedicament> PatientMedicaments { get; set; }
+        public DbSet<Visitation> Visitations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(@"Server =.; Database = HospitalDatabase; Integrated Security = True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PatientMedicament>(entity =>
+            {
+                entity.HasKey(pm => new { pm.MedicamentId, pm.PatientId });
+            });
+        }
+    }
+
+}
---
+++ Entity Framewor Core/CodeFirst/HospitalDatabase/Data/Models/PatientMedicament.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class PatientMedicament
+    {
+        [ForeignKey("Medicament")]
+        public int MedicamentId { get; set; }
+        [ForeignKey("Patient")]
+        public int PatientId { get; set; }
+        public Medicament Medicament { get; set; }
+        public Patient Patient { get; set; }
+    }
+}
