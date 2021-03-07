2e7de4543ebbc60983bbc6c1edb5f6cb01e55e93
Yavor Gledachev <jaking0111@gmail.com>
2021-03-07 15:12:20 +0200

    add fastfood asp.net web application

---
+++ Entity Framewor Core/AutoMappingObjects/FastFood/FastFood.Core/ViewModels/Orders/CreateOrderViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FastFood.Core.ViewModels.Orders
+{
+    using System.Collections.Generic;
+
+    public class CreateOrderViewModel
+    {
+        public List<CreateorderItemViewModel> Items { get; set; }
+
+        public List<CreateOrderEmployeeViewModel> Employees { get; set; }
+
+
+    }
+}
